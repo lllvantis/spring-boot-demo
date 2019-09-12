8485de5860bf74e9d913920d1e62b39953f34893
vantis <lllvantis@live.com>
2019-09-12 12:05:40 +0800

    添加注释

--- src/main/java/com/github/lllvantis/spring/boot/demo/config/RemoteUrlProperties.java
+++ src/main/java/com/github/lllvantis/spring/boot/demo/config/RemoteUrlProperties.java
@@ -3,9 +3,6 @@ package com.github.lllvantis.spring.boot.demo.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import javax.sql.DataSource;
-import java.util.List;
-
 @ConfigurationProperties("vantis.module")
 @Data
 public class RemoteUrlProperties {
