6be54093efbd4aba733ecf1ea5c8b8e1a73fd98f
vantis <lllvantis@live.com>
2019-09-12 12:03:00 +0800

    增加注释

--- src/main/java/com/github/lllvantis/spring/boot/demo/controller/WelcomeController.java
+++ src/main/java/com/github/lllvantis/spring/boot/demo/controller/WelcomeController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+	// final 表明自身不会对注入的 Bean 变更
 	private final WelcomeService welcomeService;
 
+	// 构造器注入 表示自身依赖于其他 Bean 才可以实例化
 	public WelcomeController(WelcomeService welcomeService) {
 		this.welcomeService = welcomeService;
 	}
